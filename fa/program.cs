d85309ac916d43d8394ff2ebbc9d5b9dcdf6ade7
Vadim Tretyakov <vadim.and.tretyakov@gmail.com>
2024-05-12 16:37:07 +0300

    *

--- fa/program.cs
+++ fa/program.cs
@@ -16,25 +16,98 @@ namespace fans
 
   public class FA1
   {
+    public static readonly State a = new State { Name = "a", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+    public static readonly State b = new State { Name = "b", IsAcceptState = true, Transitions = new Dictionary<char, State>() };
+    public static readonly State с = new State { Name = "с", IsAcceptState = true, Transitions = new Dictionary<char, State>() };
+
+    public static readonly State InitialState = a;
+    public FA1()
+    {
+      a.Transitions['0'] = b;
+      a.Transitions['1'] = a;
+
+      b.Transitions['1'] = с;
+      
+      с.Transitions['1'] = с;
+    }
+
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+      State current = InitialState;
+      foreach (var c in s)
+      {
+        if (!current.Transitions.TryGetValue(c, out current)) return false;
+      }
+      return current.IsAcceptState;
     }
   }
 
   public class FA2
   {
+    public static readonly State a = new State() { Name = "a", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+    public static readonly State b = new State() { Name = "b", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+    public static readonly State c = new State() { Name = "c", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+    public static readonly State d = new State() { Name = "d", IsAcceptState = true, Transitions = new Dictionary<char, State>() };
+
+    public static readonly State InitialState = a;
+
+    public FA2()
+    {
+      a.Transitions['0'] = b;
+      a.Transitions['1'] = c;
+
+      b.Transitions['0'] = a;
+      b.Transitions['1'] = d;
+
+      c.Transitions['0'] = d;
+      c.Transitions['1'] = a;
+
+      d.Transitions['0'] = c;
+      d.Transitions['1'] = b;
+    }
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+      State current = InitialState;
+      foreach (var c in s)
+      {
+        if (!current.Transitions.TryGetValue(c, out current)) return false;
+      }
+      return current.IsAcceptState;
     }
   }
-  
+
   public class FA3
   {
+    public static readonly State a = new State() { Name = "a", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+    public static readonly State b = new State() { Name = "b", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+    public static readonly State c = new State() { Name = "c", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+    public static readonly State d = new State() { Name = "d", IsAcceptState = true, Transitions = new Dictionary<char, State>() };
+
+    public static readonly State InitialState = a;
+
+    public FA3()
+    {
+      a.Transitions['0'] = b;
+      a.Transitions['1'] = c;
+
+      b.Transitions['0'] = a;
+      b.Transitions['1'] = c;
+
+      c.Transitions['0'] = a;
+      c.Transitions['1'] = d;
+
+      d.Transitions['0'] = d;
+      d.Transitions['1'] = d;
+    }
+
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+      State current = InitialState;
+      foreach (var c in s)
+      {
+        if (!current.Transitions.TryGetValue(c, out current)) return false;
+      }
+      return current.IsAcceptState;
     }
   }
 
